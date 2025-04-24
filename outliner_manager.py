d51a80a87dd2defe8b840be289c97fc54bd864b1
cereaIx <alicepopilliu@gmail.com>
2025-04-24 17:29:59 -0400

    initialized code

--- outliner_manager.py
+++ outliner_manager.py
@@ -1,5 +1,48 @@
+import sys
 from maya.cmds import cmds
-from maya.api import OpenMaya
+import maya.api.OpenMaya as om
 import maya.api.OpenMayaUI as OpenMayaUI
 
-maya_useNewAPI = True
+def maya_useNewAPI():
+    pass
+
+
+# command
+class Py2HelloWorldCmd(om.MPxCommand):
+    kPluginCmdName = "py2HelloWorld"
+
+    def __init__(self):
+        om.MPxCommand.__init__(self)
+
+    @staticmethod
+    def cmdCreator():
+        return Py2HelloWorldCmd()
+
+    def doIt(self, args):
+        print ("Hello World!")
+
+
+# Initialize the plug-in
+def initializePlugin(plugin):
+    pluginFn = om.MFnPlugin(plugin)
+    try:
+        pluginFn.registerCommand(
+            Py2HelloWorldCmd.kPluginCmdName, Py2HelloWorldCmd.cmdCreator
+        )
+    except:
+        sys.stderr.write(
+            "Failed to register command: %s\n" % Py2HelloWorldCmd.kPluginCmdName
+        )
+    raise
+
+
+# Uninitialize the plug-in
+def uninitializePlugin(plugin):
+    pluginFn = om.MFnPlugin(plugin)
+    try:
+        pluginFn.deregisterCommand(Py2HelloWorldCmd.kPluginCmdName)
+    except:
+        sys.stderr.write(
+            "Failed to unregister command: %s\n" % Py2HelloWorldCmd.kPluginCmdName
+        )
+    raise
