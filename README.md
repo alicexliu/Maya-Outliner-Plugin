029ee0a15e6b04d2c73e0ad35a4eee38e9f44c58
cereaIx <alicepopilliu@gmail.com>
2025-04-25 10:25:17 -0400

    added basic ui window and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Maya Outliner Plugin
- Maya outliner manager that allows users to easily color and rename objects 
+
+Description
+------------
+Maya outliner manager that allows users to easily color and rename objects 
+
+Explanation of code structure
+------------
+
+
+Instructions to install and run the project
+------------
+1. Download Autodesk Maya (Free for students)
+2. Set up the build environment/Maya Devkit (we used Maya 2025 Update 3 win64 DevKit)
+    * [Windows](https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=Maya_DEVHELP_Setting_up_your_build_Windows_environment_64_bit_html)
+    * [Linux](https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=Maya_DEVHELP_Setting_up_your_build_Linux_environment_html)
+    * [MacOS](https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=Maya_DEVHELP_Setting_up_your_build_Mac_OS_X_environment_html)
+3. Load the plug-in: [Instructions](https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=Maya_DEVHELP_LoadingAndUnloadingPlugins_Loading_Samples_Plug_ins_Into_Maya_html)
+4. Open the Maya Script Editor, navigate to the Python tab, and run:
+```
+import maya.cmds as mc
+mc.py2OutlinerManager()
+```
--- py2outlinerManager.py
+++ py2outlinerManager.py
@@ -1,42 +1,69 @@
 import sys
+import maya.cmds as mc
 import maya.api.OpenMaya as om
-import maya.api.OpenMayaUI as OpenMayaUI
 
 def maya_useNewAPI():
     pass
 
+def createUIWindow():
+    if mc.window('window', exists=True):
+        mc.deleteUI('window')
+
+    # create window
+    mc.window('window', title="Outliner Manager", widthHeight=(100, 100))
+
+    # add UI
+    mc.columnLayout( adjustableColumn=True )
+
+    # default rgb color buttons
+    red = mc.button( bgc=(1, 0, 0) )
+    green = mc.button( bgc=(0, 1, 0) )
+    blue = mc.button( bgc=(0, 0, 1) )
+
+    # Custom colors and palettes
+
+    # show window
+    mc.showWindow('window')
+
+# get a button's background color
+def getButtonColor(button):
+    return mc.button(button, query=True, bgc=True)
+
+# set a button's background color
+def setColor(button, color):
+    mc.button(button, edit=True, bgc=color)
 
 # command
-class Py2HelloWorldCmd(om.MPxCommand):
-    kPluginCmdName = "py2outlinerManager"
+class Py2OutlinerManagerCmd(om.MPxCommand):
+    kPluginCmdName = "py2OutlinerManager"
 
     def __init__(self):
         om.MPxCommand.__init__(self)
 
     @staticmethod
     def cmdCreator():
-        return Py2HelloWorldCmd()
+        return Py2OutlinerManagerCmd()
 
     def doIt(self, args):
-        print ("Hello World!")
+        createUIWindow()
 
-
+# initialize plug=in
 def initializePlugin(plugin):
     pluginFn = om.MFnPlugin(plugin)
     try:
         pluginFn.registerCommand(
-            Py2HelloWorldCmd.kPluginCmdName,
-            Py2HelloWorldCmd.cmdCreator
+            Py2OutlinerManagerCmd.kPluginCmdName,
+            Py2OutlinerManagerCmd.cmdCreator
         )
     except Exception as e:
-        sys.stderr.write(f"Failed to register command: {Py2HelloWorldCmd.kPluginCmdName}\n")
+        sys.stderr.write(f"Failed to register command: {Py2OutlinerManagerCmd.kPluginCmdName}\n")
         raise e
 
-
+# uninitialize plug=in
 def uninitializePlugin(plugin):
     pluginFn = om.MFnPlugin(plugin)
     try:
-        pluginFn.deregisterCommand(Py2HelloWorldCmd.kPluginCmdName)
+        pluginFn.deregisterCommand(Py2OutlinerManagerCmd.kPluginCmdName)
     except Exception as e:
-        sys.stderr.write(f"Failed to unregister command: {Py2HelloWorldCmd.kPluginCmdName}\n")
+        sys.stderr.write(f"Failed to unregister command: {Py2OutlinerManagerCmd.kPluginCmdName}\n")
         raise e
