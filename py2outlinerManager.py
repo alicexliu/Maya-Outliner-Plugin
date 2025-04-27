15dc3ba94bfc5a4729ed4f395a2be2af48e32c74
cereaIx <alicepopilliu@gmail.com>
2025-04-27 11:36:28 -0400

    updated ui window

--- py2outlinerManager.py
+++ py2outlinerManager.py
@@ -12,36 +12,48 @@ def createUIWindow():
     # create window
     mc.window('window', title="Outliner Manager", widthHeight=(100, 100))
 
+    # layout
+    mc.columnLayout(adjustableColumn=True, rowSpacing=10, columnAlign="center")
+
     # add UI
     # renamer
-    mc.columnLayout( adjustableColumn=True )
     renamer_text = mc.text( label="Auto Renamer", align="left")
     name = mc.textField()
     name_field = mc.textField( name , edit=True, enterCommand=('') )
+
+    mc.separator(height=10, style='in')
 
     # default colors
     # mc.columnLayout( adjustableColumn=True )
     dcolors_text = mc.text( label="Default Colors", align="left" )
 
     # default rgb color buttons
-    mc.gridLayout()
+    mc.gridLayout(numberOfColumns=3, cellWidthHeight=(80, 40))
     red = mc.button( label="", bgc=(1, 0, 0) )
     green = mc.button( label="", bgc=(0, 1, 0) )
     blue = mc.button( label="", bgc=(0, 0, 1) )
+
+    mc.setParent('..')
+    mc.separator(height=10, style='in')
 
     # custom colors and palettes
     mc.columnLayout( adjustableColumn=True )
     ccolors_text = mc.text( label="Custom Colors/Palettes", align="left" )
 
     # custom color buttons (TODO: implement logic)
-    mc.gridLayout()
-    c1 = mc.button( label="", bgc=(1, 0, 0) )
-    c2 = mc.button( label="", bgc=(0, 1, 0) )
-    c3 = mc.button( label="", bgc=(0, 0, 1) )
+    mc.gridLayout(numberOfColumns=3, cellWidthHeight=(80, 40))
+    c1 = mc.button( label="", bgc=(1, 0, 1) )
+    c2 = mc.button( label="", bgc=(1, 1, 0) )
+    c3 = mc.button( label="", bgc=(0, 1, 1) )
+    c4 = mc.button( label="", bgc=(1, 0, 0) )
+    c5 = mc.button( label="", bgc=(0, 0, 1) )
+    c6 = mc.button( label="", bgc=(0, 1, 0) )
+
+    mc.setParent('..')
 
     # show window
     mc.showWindow('window')
-    
+
 # get a button's background color
 def getButtonColor(button):
     return mc.button(button, query=True, bgc=True)
